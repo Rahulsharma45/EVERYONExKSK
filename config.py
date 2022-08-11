a9100e4cc8402e595bdb7518b7f138e7cf04d7f1
Rahulsharma45 <104455718+Rahulsharma45@users.noreply.github.com>
2022-08-11 23:39:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c5952790fa8235f499749.jpg")
 START_IMG = getenv("START_IMG")
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/FallenXBots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://BGMIxCHATS")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/KSKxCHEATS")
 
 STRING_SESSION = getenv("STRING_SESSION", None)
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
