978d74f900942abd2d054ed1910e271f3badf82e
Rahulsharma45 <104455718+Rahulsharma45@users.noreply.github.com>
2022-08-13 03:05:35 +0530

    Update Start.py

--- FallenMusic/Modules/Start.py
+++ FallenMusic/Modules/Start.py
@@ -81,7 +81,7 @@ async def gstart(_, message: Message):
             ],
             [
                 InlineKeyboardButton(
-                    text="☁ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ☁", url="https://github.com/AnonymousR1025/FallenMusic"
+                    text="☁ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ☁", url="https://te.legra.ph/file/bdada8e6adf6df981765b.jpg"
                 ),
             ],
         ]
